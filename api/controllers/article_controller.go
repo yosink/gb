7add0a661bb3ff58eeb6316ffbd7a5f0b9abbed8
yuxinke <yuxinke@ecmoho.com>
2020-05-12 19:07:07 +0800

    import validator

--- api/controllers/article_controller.go
+++ api/controllers/article_controller.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"strconv"
 
+	valid "github.com/asaskevich/govalidator"
+
 	"github.com/jinzhu/gorm"
 
 	"github.com/gin-gonic/gin"
@@ -52,4 +54,33 @@ func GetArticle(c *gin.Context) {
 	}
 	a := app.Gin{C: c}
 	a.Response(http.StatusOK, code, data)
+}
+
+type AddArticleRequest struct {
+	CID             uint   `valid:"int,required"`
+	UserID          uint   `valid:"int,required"`
+	Slug            string `valid:"stringlength(3|20),optional"`
+	Title           string `valid:"required,stringlength(2|100)"`
+	Subtitle        string `valid:"stringlength(2|50),optional"`
+	Content         string `valid:"required"`
+	PageImage       string `valid:"required,url"`
+	MetaDescription string `valid:"optional"`
+	Recommend       uint8  `valid:"range(0|1),required"`
+	Sort            uint   `valid:"range(0|255),optional"`
+	ViewCount       uint   `valid:"int"`
+}
+
+func AddArticle(c *gin.Context) {
+	var (
+		appG = app.Gin{C: c}
+		form AddArticleRequest
+		code = e.Success
+	)
+	_ = c.Bind(&form)
+	valid.SetFieldsRequiredByDefault(true)
+	_, err := valid.ValidateStruct(form)
+	if err != nil {
+		code = e.InvalidParams
+	}
+	appG.Response(http.StatusOK, code, nil)
 }
